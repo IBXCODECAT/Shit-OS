ef91317d0f1149b0f8bfcf93ddcfcb457f9c7eb3
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-07-19 21:24:47 -0500

    Move Cpp/H to new directory

--- src/compile.bat
+++ src/compile.bat
@@ -1,7 +1,7 @@
 nasm ./asm/boot.asm -f bin -o boot.bin
 nasm ./asm/extended.asm -f elf64 -o extended.o
 
-wsl $WSLENV/x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "krnl.cpp" -o "krnl.o"
+wsl $WSLENV/x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "./cpp/krnl.cpp" -o "krnl.o"
 
 wsl $WSLENV/x86_64-elf-ld -T"linker.ld"
 
